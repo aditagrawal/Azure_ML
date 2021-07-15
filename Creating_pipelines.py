b02f2c0242b03b5878026124e8a51f9cec353970
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-15 17:00:36 +0530

    Update Creating_pipelines.py

--- Creating_pipelines.py
+++ Creating_pipelines.py
@@ -1,6 +1,9 @@
+# Link- https://docs.microsoft.com/en-us/learn/modules/create-pipelines-in-aml/
+
 from azureml.pipeline.steps import PythonScriptStep
 
-# Defining steps
+### Defining Pipeline steps
+
 # Step to run a Python script
 step1 = PythonScriptStep(name = 'prepare data',
                          source_directory = 'scripts',
@@ -13,7 +16,8 @@ step2 = PythonScriptStep(name = 'train model',
                          script_name = 'train_model.py',
                          compute_target = 'aml-cluster')
 
-# Creating a pipeline using steps
+### Creating a pipeline using steps
+
 from azureml.pipeline.core import Pipeline
 from azureml.core import Experiment
 
@@ -23,3 +27,32 @@ train_pipeline = Pipeline(workspace = ws, steps = [step1,step2])
 # Create an experiment and run the pipeline
 experiment = Experiment(workspace = ws, name = 'training-pipeline')
 pipeline_run = experiment.submit(train_pipeline)
+
+### Pass data between pipeline steps
+
+from azureml.data import OutputFileDatasetConfig
+from azureml.pipeline.steps import PythonScriptStep, EstimatorStep
+
+# Get a dataset for the initial data
+raw_ds = Dataset.get_by_name(ws, 'raw_dataset')
+
+# Define a PipelineData object to pass data between steps
+data_store = ws.get_default_datastore()
+prepped_data = OutputFileDatasetConfig('prepped')
+
+# Step to run a Python script
+step1 = PythonScriptStep(name = 'prepare data',
+                         source_directory = 'scripts',
+                         script_name = 'data_prep.py',
+                         compute_target = 'aml-cluster',
+                         # Script arguments include PipelineData
+                         arguments = ['--raw-ds', raw_ds.as_named_input('raw_data'),
+                                      '--out_folder', prepped_data])
+
+# Step to run an estimator
+step2 = PythonScriptStep(name = 'train model',
+                         source_directory = 'scripts',
+                         script_name = 'train_model.py',
+                         compute_target = 'aml-cluster',
+                         # Pass as script argument
+                         arguments=['--training-data', prepped_data.as_input()])
