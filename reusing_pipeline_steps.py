24a4abe617cdfe6a78cc88de8fd6c9cb2b358e89
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-15 17:05:51 +0530

    Create reusing_pipeline_steps.py

---
+++ reusing_pipeline_steps.py
@@ -0,0 +1,12 @@
+step1 = PythonScriptStep(name = 'prepare data',
+                         source_directory = 'scripts',
+                         script_name = 'data_prep.py',
+                         compute_target = 'aml-cluster',
+                         runconfig = run_config,
+                         inputs=[raw_ds.as_named_input('raw_data')],
+                         outputs=[prepped_data],
+                         arguments = ['--folder', prepped_data]),
+                         # Disable step reuse
+                         allow_reuse = False)
+                         
+pipeline_run = experiment.submit(train_pipeline, regenerate_outputs=True)
