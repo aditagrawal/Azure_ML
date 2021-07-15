3fcda98801b2729689d0b9f015338d0754c9fdd7
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-15 19:27:25 +0530

    Update 1_Create_batch_inference_pipeline.py

--- 4a_Deploy_batch_inference_pipeline/1_Create_batch_inference_pipeline.py
+++ 4a_Deploy_batch_inference_pipeline/1_Create_batch_inference_pipeline.py
@@ -1,1 +1,108 @@
+### 1. Register a Model
+
+from azureml.core import Model
+
+classification_model = Model.register(workspace=your_workspace,
+                                      model_name='classification_model',
+                                      model_path='model.pkl', # local path
+                                      description='A classification model')
+
+# Alternatively, if you have a reference to the Run used to train the model, you can use its register_model method as shown in the following example code:
+
+run.register_model( model_name='classification_model',
+                    model_path='outputs/model.pkl', # run outputs path
+                    description='A classification model')
+
+
+### 2. Create a scoring script- it must include 2 functions- init and run
+
+import os
+import numpy as np
+from azureml.core import Model
+import joblib
+
+def init():
+    # Runs when the pipeline step is initialized
+    global model
+
+    # load the model
+    model_path = Model.get_model_path('classification_model')
+    model = joblib.load(model_path)
+
+def run(mini_batch):
+    # This runs for each batch
+    resultList = []
+
+    # process each file in the batch
+    for f in mini_batch:
+        # Read comma-delimited data into an array
+        data = np.genfromtxt(f, delimiter=',')
+        # Reshape into a 2-dimensional array for model input
+        prediction = model.predict(data.reshape(1, -1))
+        # Append prediction to results
+        resultList.append("{}: {}".format(os.path.basename(f), prediction[0]))
+    return resultList
+  
+### 3. Create a pipeline with a ParallelRunStep
+
+# Azure Machine Learning provides a type of pipeline step specifically for performing parallel batch inferencing. Using the ParallelRunStep class, you can read batches of files from a File dataset and write the processing output to a OutputFileDatasetConfig. Additionally, you can set the output_action setting for the step to "append_row", which will ensure that all instances of the step being run in parallel will collate their results to a single output file named parallel_run_step.txt:
+
+from azureml.pipeline.steps import ParallelRunConfig, ParallelRunStep
+from azureml.data import OutputFileDatasetConfig
+from azureml.pipeline.core import Pipeline
+
+# Get the batch dataset for input
+batch_data_set = ws.datasets['batch-data']
+
+# Set the output location
+default_ds = ws.get_default_datastore()
+output_dir = OutputFileDatasetConfig(name='inferences')
+
+# Define the parallel run step step configuration
+parallel_run_config = ParallelRunConfig(
+    source_directory='batch_scripts',
+    entry_script="batch_scoring_script.py",
+    mini_batch_size="5",
+    error_threshold=10,
+    output_action="append_row",
+    environment=batch_env,
+    compute_target=aml_cluster,
+    node_count=4)
+
+# Create the parallel run step
+parallelrun_step = ParallelRunStep(
+    name='batch-score',
+    parallel_run_config=parallel_run_config,
+    inputs=[batch_data_set.as_named_input('batch_data')],
+    output=output_dir,
+    arguments=[],
+    allow_reuse=True
+)
+# Create the pipeline
+pipeline = Pipeline(workspace=ws, steps=[parallelrun_step])
+
+### 4. Run the pipeline and retrieve the step output
+
+from azureml.core import Experiment
+
+# Run the pipeline as an experiment
+pipeline_run = Experiment(ws, 'batch_prediction_pipeline').submit(pipeline)
+pipeline_run.wait_for_completion(show_output=True)
+
+# Get the outputs from the first (and only) step
+prediction_run = next(pipeline_run.get_children())
+prediction_output = prediction_run.get_output_data('inferences')
+prediction_output.download(local_path='results')
+
+# Find the parallel_run_step.txt file
+for root, dirs, files in os.walk('results'):
+    for file in files:
+        if file.endswith('parallel_run_step.txt'):
+            result_file = os.path.join(root,file)
+
+# Load and display the results
+df = pd.read_csv(result_file, delimiter=":", header=None)
+df.columns = ["File", "Prediction"]
+print(df)
+
 
