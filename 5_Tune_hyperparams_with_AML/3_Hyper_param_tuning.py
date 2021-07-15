52b827402ad46d9c4a995b4d38d58b16f433e894
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-15 20:21:03 +0530

    Update 3_Hyper_param_tuning.py

--- 5_Tune_hyperparams_with_AML/3_Hyper_param_tuning.py
+++ 5_Tune_hyperparams_with_AML/3_Hyper_param_tuning.py
@@ -1,5 +1,7 @@
 ### 1. Creating a training script for hyperparameter tuning
 
+#Script must include 2 things- 1. Include an argument for each hyperparameter you want to vary. 2. Log the target performance metric. This enables the hyperdrive run to evaluate the performance of the child runs it initiates, and identify the one that produces the best performing model.
+
 import argparse
 import joblib
 from azureml.core import Run
@@ -39,3 +41,34 @@ joblib.dump(value=model, filename='outputs/model.pkl')
 
 run.complete()
 
+
+### Configuring and running a hyperdrive experiment
+
+from azureml.core import Experiment
+from azureml.train.hyperdrive import HyperDriveConfig, PrimaryMetricGoal
+
+# Assumes ws, script_config and param_sampling are already defined
+
+hyperdrive = HyperDriveConfig(run_config=script_config,
+                              hyperparameter_sampling=param_sampling,
+                              policy=None,
+                              primary_metric_name='Accuracy',
+                              primary_metric_goal=PrimaryMetricGoal.MAXIMIZE,
+                              max_total_runs=6,
+                              max_concurrent_runs=4)
+
+experiment = Experiment(workspace = ws, name = 'hyperdrive_training')
+hyperdrive_run = experiment.submit(config=hyperdrive)
+
+### Monitoring and reviewing hyperdrive runs
+
+# The experiment will initiate a child run for each hyperparameter combination to be tried, and you can retrieve the logged metrics these runs using the following code:
+for child_run in run.get_children():
+    print(child_run.id, child_run.get_metrics())
+
+# You can also list all runs in descending order of performance like this:    
+for child_run in hyperdrive_run.get_children_sorted_by_primary_metric():
+    print(child_run)
+
+# To retrieve the best performing run, you can use the following code:   
+best_run = hyperdrive_run.get_best_run_by_primary_metric()
