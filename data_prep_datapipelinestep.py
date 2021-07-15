eb49f9ea7ece7929668fa96460fd6498d32da649
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-15 17:04:36 +0530

    Create data_prep_datapipelinestep.py

---
+++ data_prep_datapipelinestep.py
@@ -0,0 +1,24 @@
+from azureml.core import Run
+import argparse
+import os
+
+# Get the experiment run context
+run = Run.get_context()
+
+# Get arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--raw-ds', type=str, dest='raw_dataset_id')
+parser.add_argument('--out_folder', type=str, dest='folder')
+args = parser.parse_args()
+output_folder = args.folder
+
+# Get input dataset as dataframe
+raw_df = run.input_datasets['raw_data'].to_pandas_dataframe()
+
+# code to prep data (in this case, just select specific columns)
+prepped_df = raw_df[['col1', 'col2', 'col3']]
+
+# Save prepped data to the PipelineData location
+os.makedirs(output_folder, exist_ok=True)
+output_path = os.path.join(output_folder, 'prepped_data.csv')
+prepped_df.to_csv(output_path)
