b50af483a4e2cd8a09b42da9da0c3a98d899d0fd
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-15 17:44:31 +0530

    Update 1_Creating_pipelines.py

--- 3_Pipelines/1_Creating_pipelines.py
+++ 3_Pipelines/1_Creating_pipelines.py
@@ -90,3 +90,28 @@ response = requests.post(rest_endpoint,
 run_id = response.json()["Id"]
 #print(run_id)
 
+
+### Defining Paramters for a Pipeline
+
+from azureml.pipeline.core.graph import PipelineParameter
+
+reg_param = PipelineParameter(name='reg_rate', default_value=0.01)
+
+...
+
+step2 = PythonScriptStep(name = 'train model',
+                         source_directory = 'scripts',
+                         script_name = 'data_prep.py',
+                         compute_target = 'aml-cluster',
+                         # Pass parameter as script argument
+                         arguments=['--in_folder', prepped_data,
+                                    '--reg', reg_param],
+                         inputs=[prepped_data])
+
+### Running a Pipeline with a parameter
+
+response = requests.post(rest_endpoint,
+                         headers=auth_header,
+                         json={"ExperimentName": "run_training_pipeline",
+                               "ParameterAssignments": {"reg_rate": 0.1}})
+
