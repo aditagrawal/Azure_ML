88845f1cd0d935883307f49fc11afb82e4076417
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-15 18:34:40 +0530

    Create 2_Consume_real_time_inference_service.py

---
+++ 4_Deploy_inferencing/2_Consume_real_time_inference_service.py
@@ -0,0 +1,91 @@
+## Below is data in json format
+
+{
+  "data":[
+      [0.1,2.3,4.1,2.0], // 1st case
+      [0.2,1.8,3.9,2.1],  // 2nd case,
+      ...
+  ]
+}
+
+# Code for real time inference
+
+import json
+
+# An array of new data cases
+x_new = [[0.1,2.3,4.1,2.0],
+         [0.2,1.8,3.9,2.1]]
+
+# Convert the array to a serializable list in a JSON document
+json_data = json.dumps({"data": x_new})
+
+# Call the web service, passing the input data
+response = service.run(input_data = json_data)
+
+# Get the predictions
+predictions = json.loads(response)
+
+# Print the predicted class for each case.
+for i in range(len(x_new)):
+    print (x_new[i], predictions[i])
+    
+
+### Another option- Use a REST endpoint to consumer real tiime infrernece
+
+endpoint = service.scoring_uri
+print(endpoint)
+
+import requests
+import json
+
+# An array of new data cases
+x_new = [[0.1,2.3,4.1,2.0],
+         [0.2,1.8,3.9,2.1]]
+
+# Convert the array to a serializable list in a JSON document
+json_data = json.dumps({"data": x_new})
+
+# Set the content type in the request headers
+request_headers = { 'Content-Type':'application/json' }
+
+# Call the service
+response = requests.post(url = endpoint,
+                         data = json_data,
+                         headers = request_headers)
+
+# Get the predictions from the JSON response
+predictions = json.loads(response.json())
+
+# Print the predicted class for each case.
+for i in range(len(x_new)):
+    print (x_new[i]), predictions[i] )
+    
+### Authentication 
+
+primary_key, secondary_key = service.get_keys()
+
+import requests
+import json
+
+# An array of new data cases
+x_new = [[0.1,2.3,4.1,2.0],
+         [0.2,1.8,3.9,2.1]]
+
+# Convert the array to a serializable list in a JSON document
+json_data = json.dumps({"data": x_new})
+
+# Set the content type in the request headers
+request_headers = { "Content-Type":"application/json",
+                    "Authorization":"Bearer " + key_or_token }
+
+# Call the service
+response = requests.post(url = endpoint,
+                         data = json_data,
+                         headers = request_headers)
+
+# Get the predictions from the JSON response
+predictions = json.loads(response.json())
+
+# Print the predicted class for each case.
+for i in range(len(x_new)):
+    print (x_new[i]), predictions[i] )
