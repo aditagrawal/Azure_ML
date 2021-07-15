9da399349120a90a40397c59a2d3bb18e179bd71
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-15 16:52:19 +0530

    Update and rename Creating_pipelines to Creating_pipelines.py

--- Creating_pipelines.py
+++ Creating_pipelines.py
@@ -1,5 +1,6 @@
 from azureml.pipeline.steps import PythonScriptStep
 
+# Defining steps
 # Step to run a Python script
 step1 = PythonScriptStep(name = 'prepare data',
                          source_directory = 'scripts',
@@ -11,3 +12,14 @@ step2 = PythonScriptStep(name = 'train model',
                          source_directory = 'scripts',
                          script_name = 'train_model.py',
                          compute_target = 'aml-cluster')
+
+# Creating a pipeline using steps
+from azureml.pipeline.core import Pipeline
+from azureml.core import Experiment
+
+# Construct the pipeline
+train_pipeline = Pipeline(workspace = ws, steps = [step1,step2])
+
+# Create an experiment and run the pipeline
+experiment = Experiment(workspace = ws, name = 'training-pipeline')
+pipeline_run = experiment.submit(train_pipeline)
