fc37df2608338b2f4ad970629b77bac2307361c9
adit agrawal <aditagrawal.iitbhu@gmail.com>
2021-07-16 16:36:13 +0530

    Update 1_Creating_data_drift_monitor.py

--- 9_Monitor_Data_Drift/1_Creating_data_drift_monitor.py
+++ 9_Monitor_Data_Drift/1_Creating_data_drift_monitor.py
@@ -1,1 +1,30 @@
+### Creating data drift monitor
+
+from azureml.datadrift import DataDriftDetector
+
+monitor = DataDriftDetector.create_from_datasets(workspace=ws,
+                                                 name='dataset-drift-detector',
+                                                 baseline_data_set=train_ds,
+                                                 target_data_set=new_data_ds,
+                                                 compute_target='aml-cluster',
+                                                 frequency='Week',
+                                                 feature_list=['age','height', 'bmi'],
+                                                 latency=24)
+
+### After creating the dataset monitor, you can backfill to immediately compare the baseline dataset to existing data in the target dataset, as shown in the following example, which backfills the monitor based on weekly changes in data for the previous six weeks:
+
+import datetime as dt
+
+backfill = monitor.backfill( dt.datetime.now() - dt.timedelta(weeks=6), dt.datetime.now())
+
+### Configure Alerts
+# The following code shows an example of scheduling a data drift monitor to run every week, and send an alert if the drift magnitude is greater than 0.3:
+
+alert_email = AlertConfiguration('data_scientists@contoso.com')
+monitor = DataDriftDetector.create_from_datasets(ws, 'dataset-drift-detector', 
+                                                 baseline_data_set, target_data_set,
+                                                 compute_target=cpu_cluster,
+                                                 frequency='Week', latency=2,
+                                                 drift_threshold=.3,
+                                                 alert_configuration=alert_email)
 
